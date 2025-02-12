f3651073d3d795dc2402fd7a960f471ad171971c
Jesus Bejar <israelbejarii@gmail.com>
2025-02-11 20:34:48 -0700

    finished some()

---
+++ some/scripts.js
@@ -0,0 +1,13 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const numPresentTrue = array.some(num => {
+    return num > 9
+})
+
+console.log(numPresentTrue)
+
+const numPresentFalse = array.some(num => {
+    return num == 12
+})
+
+console.log(numPresentFalse)
