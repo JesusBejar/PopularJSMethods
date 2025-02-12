b2f3b7771a269fdaf19432de0268720396790845
Jesus Bejar <israelbejarii@gmail.com>
2025-02-11 19:56:56 -0700

    finished find()

--- forEach/scripts.js
+++ forEach/scripts.js
@@ -3,7 +3,7 @@ const list = document.querySelector('#list')
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 array.forEach(num => {
-    console.log(num)
+    // console.log(num)
     const numberDisplay = document.createElement('p')
     numberDisplay.innerHTML = `This is my number - ${num}`
     list.appendChild(numberDisplay)
