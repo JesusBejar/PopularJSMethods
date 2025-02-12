17c0d76aa2edd246c00120d74ffb90d923d3003d
Jesus Bejar <israelbejarii@gmail.com>
2025-02-11 20:15:04 -0700

    finished every()

---
+++ every/scripts.js
@@ -0,0 +1,12 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const isFalse = array.every(num => {
+    return num >= 9
+})
+console.log(isFalse)
+
+array = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+const isTrue = array.every(num => {
+    return num == 2
+})
+console.log(isTrue)
